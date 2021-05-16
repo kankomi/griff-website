7e14096850ab0b65d0d1886df5a21edcde49937d
Patrick Groß <patrick@pgross.de>
2021-05-16 09:36:46 +0200

    added insta grid and particles

--- script/main.js
+++ script/main.js
@@ -1,5 +1,20 @@
-window.onload = () => {};
+const instaGridElem = document.querySelector('.insta-grid');
 
-particlesJS.load('particles', 'script/particles/config.json', () => {
-  console.log('loaded');
-});
+particlesJS.load('particles', 'script/particles/config.json');
+
+function instagramGrid({ data }) {
+  const images = data.filter((x) => x.media_type !== 'VIDEO');
+
+  for (const elem of images) {
+    const pic = document.createElement('a');
+
+    pic.className = 'box';
+    pic.setAttribute('href', elem.permalink);
+    pic.style = `
+    background: url(${elem.media_url}) no-repeat;
+    background-size: cover;
+    `;
+
+    instaGridElem.appendChild(pic);
+  }
+}
